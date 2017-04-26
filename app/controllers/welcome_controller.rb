ed38e468fafcc880d5440dd5a8a0df857400ab7e
user <sjkacir@gmail.com>
2017-04-25 21:33:06 -0400

    stylistic updates

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,9 @@
 class WelcomeController < ApplicationController
   def index
-    trips = current_user.get_all_trips
-    @upcoming_trips = trips.where('departure > ?', DateTime.now).order(departure: :asc)
-    @previous_trips = trips.where('departure < ?', DateTime.now).order(departure: :desc)
+    if logged_in?
+      trips = current_user.get_all_trips
+      @upcoming_trips = trips.where('departure > ?', DateTime.now).order(departure: :asc)
+      @previous_trips = trips.where('departure < ?', DateTime.now).order(departure: :desc)
+    end
   end
 end
