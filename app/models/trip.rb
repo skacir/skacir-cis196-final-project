970aef06419cf708f846477d1ab9106e84501b96
user <sjkacir@gmail.com>
2017-04-26 08:42:37 -0400

    validate driver

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -8,6 +8,10 @@ class Trip < ApplicationRecord
 
   validates :available_seats, numericality: { greater_than: -1 }
 
+  validates :driver_id, numericality: { only_integer: true }, :if => :driver_id
+
+  validate :validate_driver_id
+
   def seats_available?
     return true if !available_seats || available_seats > users.size
     false
@@ -19,5 +23,12 @@ class Trip < ApplicationRecord
 
   def future_trip?
     departure > DateTime.now
+  end
+
+  private
+
+  def validate_driver_id
+    errors.add(:driver_id, "is invalid") unless User.exists?(self.driver_id)
+    errors.add(:driver_id, "does not have a car") unless User.exists?(id: driver_id, car: true)
   end
 end
